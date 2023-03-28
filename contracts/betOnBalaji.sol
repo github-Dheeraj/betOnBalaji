ae837967c183da077439827c8e0fc56c5b805b6e
qedk <1994constant@gmail.com>
2023-03-28 14:50:21 +0700

    feat: some rework

--- contracts/betOnBalaji.sol
+++ contracts/betOnBalaji.sol
@@ -1,45 +1,56 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "hardhat/console.sol";
 
-contract BettingPlatform {
-    IERC20 public usdc;
-    AggregatorV3Interface public priceFeed;
-    uint256 public deadline;
+contract BetOnBalaji {
+    using SafeERC20 for IERC20;
+
+    struct Status {
+        bool isSettled;
+        BetType winningSide;
+    }
+
+    IERC20 private immutable usdc;
+    AggregatorV3Interface private immutable priceFeed;
+    uint256 private immutable deadline;
 
     enum BetType {
         ProBalaji,
-        ProBanks,
-        Invalid
+        ProBanks
     }
-    mapping(BetType => mapping(address => uint256)) public bets;
-    mapping(BetType => uint256) public totalBets;
-    BetType public winningSide = BetType.Invalid;
+    mapping(BetType => mapping(address => uint256)) private bets;
+    mapping(BetType => uint256) private totalBets;
+    Status private status;
 
-    uint256 public constant BONUS_FACTOR_MIN = 1 ether;
-    uint256 public constant BONUS_FACTOR_MAX = 12 * (10 ** 17); // 1.2 ether
+    uint256 private constant BONUS_FACTOR_MIN = 1 ether;
+    uint256 private constant BONUS_FACTOR_MAX = 12 * (10 ** 17); // 1.2 ether
+
+    error BettingClosed();
+    error BettingOpen();
+    error InvalidAmount();
+    error InvalidPrice();
 
     event BetPlaced(
         address indexed user,
-        BetType betType,
+        BetType indexed betType,
         uint256 amount,
         uint256 effectiveAmount
     );
-    event BetsResolved(uint btcPrice, BetType winningSide);
+    event BetSettled(int256 indexed btcPrice, BetType indexed winningSide);
     event RewardClaimed(address indexed user, uint256 reward);
 
     //duration in seconds
     //_priceFeed, chainlink priceFeed address for specific chain
     //_usdcAddress is on specific chain /usdc address / Accepting token address
     constructor(
-        address _usdcAddress,
+        IERC20 _usdc,
         AggregatorV3Interface _priceFeed,
         uint256 duration
     ) {
-        usdc = IERC20(_usdcAddress);
+        usdc = _usdc;
         priceFeed = _priceFeed;
         deadline = block.timestamp + duration;
     }
@@ -49,70 +60,62 @@ contract BettingPlatform {
             return BONUS_FACTOR_MIN;
         }
 
-        uint256 remainingTime = deadline - block.timestamp;
-        uint256 totalTime = deadline - (block.timestamp - 90 days);
-        uint256 bonusFactorRange = BONUS_FACTOR_MAX - BONUS_FACTOR_MIN;
-
-        uint256 bonusFactor = BONUS_FACTOR_MIN +
-            (remainingTime * bonusFactorRange) /
-            totalTime;
-        console.log(bonusFactor);
-        return bonusFactor;
+        return BONUS_FACTOR_MIN +
+            ((deadline - block.timestamp) * (BONUS_FACTOR_MAX - BONUS_FACTOR_MIN)) /
+            (deadline - (block.timestamp - 90 days));
     }
 
     //place bet is had _betType enum value, amount in native currency
-    function placeBet(BetType _betType, uint256 _amount) external {
-        require(block.timestamp < deadline, "Betting closed");
-        require(_amount > 0, "Invalid amount");
-        require(_betType != BetType.Invalid, "Bet Type is invalid");
+    function placeBet(BetType betType, uint256 amount) external {
+        if(block.timestamp >= deadline) revert BettingClosed();
+        if(amount == 0) revert InvalidAmount();
 
         uint256 bonusFactor = calculateBonusFactor();
-        uint256 effectiveAmount = (_amount * bonusFactor) / 10 ** 6;
+        uint256 effectiveAmount = (amount * bonusFactor) / 10 ** 6;
 
-        usdc.transferFrom(msg.sender, address(this), _amount);
+        bets[betType][msg.sender] += effectiveAmount;
+        totalBets[betType] += effectiveAmount;
 
-        bets[_betType][msg.sender] += effectiveAmount;
-        totalBets[_betType] += effectiveAmount;
+        emit BetPlaced(msg.sender, betType, amount, effectiveAmount);
 
-        emit BetPlaced(msg.sender, _betType, _amount, effectiveAmount);
+        usdc.safeTransferFrom(msg.sender, address(this), amount);
     }
 
     //Single time bet settlement
     //anyone can call this fucntion
     function resolveBets() external {
-        require(block.timestamp >= deadline, "Betting still open");
-        require(winningSide == BetType.Invalid, "Bets already resolved");
+        if(block.timestamp < deadline) revert BettingOpen();
+        if(status.isSettled) revert BettingClosed();
 
         (, int256 btcPrice, , , ) = priceFeed.latestRoundData();
-        require(btcPrice > 0, "Invalid BTC price");
+        if(btcPrice == 0) revert InvalidPrice();
 
-        winningSide = uint256(btcPrice) >= 1000000 * 10 ** 8
+        BetType winningSide = btcPrice >= 1000000 * 10 ** 8
             ? BetType.ProBalaji
             : BetType.ProBanks;
-        // winningSide = uint256(_btcPrice) >= 1000000 ? BetType.ProBalaji : BetType.ProBanks;
 
-        emit BetsResolved(uint256(btcPrice), winningSide);
+        status = Status(true, winningSide);
+
+        emit BetSettled(btcPrice, winningSide);
     }
 
     //this can be called onlyOnce per user
     function claimReward() external {
-        require(winningSide != BetType.Invalid, "Bets not resolved yet");
+        Status memory currentStatus = status;
 
-        uint256 winnerBet = bets[winningSide][msg.sender];
-        require(winnerBet > 0, "No reward to claim");
+        if(block.timestamp < deadline || !currentStatus.isSettled) revert BettingOpen();
 
-        uint256 loserSideTotal = totalBets[
-            winningSide == BetType.ProBalaji
-                ? BetType.ProBanks
-                : BetType.ProBalaji
-        ];
-        uint256 reward = (winnerBet * loserSideTotal) / totalBets[winningSide];
+        uint256 winnerBet = bets[currentStatus.winningSide][msg.sender];
+        if (winnerBet == 0) revert InvalidAmount();
 
-        bets[winningSide][msg.sender] = 0;
+        uint256 loserSideTotal = totalBets[currentStatus.winningSide];
+        uint256 reward = (winnerBet * loserSideTotal) / totalBets[currentStatus.winningSide];
 
-        usdc.transfer(msg.sender, reward);
+        bets[currentStatus.winningSide][msg.sender] = 0;
 
         emit RewardClaimed(msg.sender, reward);
+
+        usdc.safeTransfer(msg.sender, reward);
     }
 
     // Additional helper functions
@@ -128,11 +131,11 @@ contract BettingPlatform {
     }
 
     function getWinningSide() external view returns (BetType) {
-        return winningSide;
+        return status.winningSide;
     }
 
     function getRemainingTime() public view returns (uint256) {
-        require(block.timestamp > deadline, "Betting closed");
+        if(block.timestamp >= deadline) return 0;
         return deadline - block.timestamp;
     }
 }
